a178677b4851fc27d69247ace48f452959058c77
ramG-reddy <venkataramgopal16@gmail.com>
2025-03-09 11:47:54 +0530

    Added required constants and components

---
+++ constants/Config.ts
@@ -0,0 +1,9 @@
+export const ORDER_OF_HIGHLIGHTS: ("up" | "down" | "right" | "left" | "action")[] = [
+  "action",
+  "up",
+  "right",
+  "down",
+  "left",
+];
+
+export const CHANGE_DELAY_ms: number = 1500;
---
+++ constants/Data.ts
@@ -0,0 +1,11 @@
+export const PRESETS = [
+  { id: "1", text: "Can You bring me a blanket" },
+  { id: "2", text: "What's for dinner today?" },
+  { id: "3", text: "How was School?" },
+  { id: "4", text: "I need some water" },
+  { id: "5", text: "Can you turn on the TV?" },
+  { id: "6", text: "Is the Coffee ready?" },
+  { id: "7", text: "Can you bring me a Snack?" },
+  { id: "8", text: "I need to use the restroom" },
+  { id: "9", text: "Can you turn off the light?" },
+];
